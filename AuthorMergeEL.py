def4771616bcefd2824f78443010c23e3df7b4e1
Simos Xenitellis <simos.lists@googlemail.com>
2010-07-19 23:32:26 +0300

    Updates, adds AuthorMergeEL, SVNContributorsEL

---
+++ AuthorMergeEL.py
@@ -0,0 +1,9 @@
+class AuthorMergeEL:
+    def __init__(self):
+        self.mapping = { 
+            'Xenitellis': 'Simos Xenitellis <simos@gnome.org>',
+            'Papadimas': 'Kostas Papadimas <pkst@gnome.org>',
+            'Charonitakis': 'Nikos Charonitakis <frolix68@gnome.org>',
+            'Petoumenou': 'Jennie Petoumenou <epetoumenou@gmail.com>'
+            }
+
---
+++ SVNContributorsEL.py
@@ -0,0 +1,15 @@
+class SVNContributorsEL:
+    def __init__(self):
+        self.mapping = { 
+            'Katsampiris': 'Giannis Katsampiris',
+            'Tsamis': 'Fotis Tsamis',
+            'Prosiniklis': 'Sterios Prosiniklis',
+            'Petoumenou': 'Jennie Petoumenou <epetoumenou@gmail.com>',
+            'Makris': 'Ilias Makris',
+            'Marios': 'Marios Zindilis',
+            'Tryfonidis': 'Tom Tryfonidis',
+            'Kotsarinis': 'Michael Kotsarinis',
+            'alefteris': 'Thanos Lefteris <alefteris@gmail.com>',
+            'Lefteris': 'Thanos Lefteris'
+            }
+
